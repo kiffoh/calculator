66935dd62448054ed93ff6484f80976e83fc9a84
Kiff <125292279+kiffoh@users.noreply.github.com>
2023-10-31 18:27:48 -0500

    Create viable functions for integers

--- calculator.js
+++ calculator.js
@@ -1,22 +1,22 @@
-//Operation functions
-function add (num1,num2) {
-    return num1 + num2;
+// Operation functions (same as before)
+function add(num1,num2) {
+    return String(Number(num1) + Number(num2));
 }
 
-function subtract (num1, num2) {
-    return num1 - num2;
+function subtract(num1, num2) {
+    return String(Number(num1) - Number(num2));
 }
 
-function multiply (num1, num2) {
-    return num1 * num2;
+function multiply(num1, num2) {
+    return String(Number(num1) * Number(num2));
 }
 
-function divide (num1, num2) {
-    if (num1 === 0) return "ERROR";
-    return num1 / num2;
+function divide(num1, num2) {
+    if (num1 === "0") return "ERROR";
+    return String(Number(num1) / Number(num2));
 }
 
-function operate (num1, num2, operator) {
+function operate(num1, num2, operator) {
     if (operator === "+") {
         return add(num1, num2);
     }
@@ -31,24 +31,56 @@ function operate (num1, num2, operator) {
     }
 }
 
-// Displaying the user input in the calculator
+// Displaying the user input in the calculator (same as before)
 const display = document.querySelector(".display")
 display.textContent = "0";
-let userInput = ""
+let userInput = "";
+let firstNumber = "";
+let secondNumber = "";
+let operator = "";
 
-const buttons = document.querySelectorAll(".button")
+const buttons = document.querySelectorAll(".button");
 
 buttons.forEach(button => {
     button.addEventListener("click", function() {
         userInput += this.textContent;
         display.textContent = userInput;
-    })
-})
+    });
+});
 
 const operators = document.querySelectorAll(".operator");
 
 operators.forEach(button => {
     button.addEventListener("click", function() {
+        if (operator === "*" && this.textContent === "-") {
+            userInput += "-"
+        }
         operator = this.textContent;
-    })
+        if (firstNumber === "") {
+            firstNumber = userInput; // Only store first number if it's not set yet
+        } else {
+            handleSecondNumberInput(); // If firstNumber is set, handle second number input
+        }
+        userInput = ""; // Reset userInput for next input
+    });
+});
+
+// Function to handle second number input
+function handleSecondNumberInput() {
+    secondNumber = userInput;
+    userInput = ""; // Reset userInput for next input
+}
+
+const equalsButton = document.querySelector(".equal");
+
+equalsButton.addEventListener("click", () => {
+    handleSecondNumberInput();
+    firstNumber = operate(firstNumber, secondNumber, operator);
+    display.textContent = firstNumber;
+})
+
+const clearButton = document.querySelector(".clear");
+
+clearButton.addEventListener("click", () => {
+
 })
