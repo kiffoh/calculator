75110e7f049e0c8f29d1cf2a3e46a529608e810e
Kiff <125292279+kiffoh@users.noreply.github.com>
2023-11-06 22:38:40 +0000

    Finish js for remove and decimal functionalilty,

--- calculator.js
+++ calculator.js
@@ -1,23 +1,47 @@
 // Operation functions (same as before)
-function add(num1,num2) {
-    return String(Number(num1) + Number(num2));
+function add(num1, num2) {
+    let ans = Number(num1) + Number(num2);
+
+    if (Number.isInteger(ans)) {
+        return String(ans);
+    } else {
+        return ans.toFixed(2);
+    }
 }
 
 function subtract(num1, num2) {
-    return String(Number(num1) - Number(num2));
+    let ans = Number(num1) - Number(num2);
+
+    if (Number.isInteger(ans)) {
+        return String(ans);
+    } else {
+        return ans.toFixed(2);
+    }
 }
 
 function multiply(num1, num2) {
-    return String(Number(num1) * Number(num2));
+    let ans = Number(num1) * Number(num2);
+
+    if (Number.isInteger(ans)) {
+        return String(ans);
+    } else {
+        return ans.toFixed(2);
+    }
 }
 
 function divide(num1, num2) {
     if (num1 === "0") return "ERROR";
-    return String(Number(num1) / Number(num2));
+    let ans = Number(num1) / Number(num2);
+
+    if (Number.isInteger(ans)) {
+        return String(ans);
+    } else {
+        return ans.toFixed(2);
+    }
 }
 
 function operate(num1, num2, operator) {
-    if (operator === "+") {
+    if (operator === "+" || operator === "--") {
         return add(num1, num2);
     }
     if (operator === "-") {
@@ -28,6 +52,12 @@ function operate(num1, num2, operator) {
     }
     if (operator === "/") {
         return divide(num1, num2);
+    }
+    if (operator === "*-") {
+        return multiply(num1,"-"+num2)
+    }
+    if (operator === "/-") {
+        return divide(num1,"-"+num2)
     }
 }
 
@@ -52,16 +82,29 @@ const operators = document.querySelectorAll(".operator");
 
 operators.forEach(button => {
     button.addEventListener("click", function() {
-        if (operator === "*" && this.textContent === "-") {
-            userInput += "-"
-        }
-        operator = this.textContent;
+        if (this.textContent === "-" && operator === "-") {
+            operator = "--";
+            // display.textContent = "-" + display.textContent;
+        } else if (this.textContent === "-" && operator === "*") {
+            operator = "*-";
+            // display.textContent = "-" + display.textContent;
+        } else if (this.textContent === "-" && operator === "/") {
+            operator = "/-";
+        } else {
+            operator = this.textContent;
+        }    
         if (firstNumber === "") {
             firstNumber = userInput; // Only store first number if it's not set yet
         } else {
             handleSecondNumberInput(); // If firstNumber is set, handle second number input
         }
         userInput = ""; // Reset userInput for next input
+
+        if (operator && firstNumber && secondNumber) {
+            firstNumber = operate(firstNumber, secondNumber, operator);
+            operator = ""
+            display.textContent = firstNumber;
+        }
     });
 });
 
@@ -71,16 +114,44 @@ function handleSecondNumberInput() {
     userInput = ""; // Reset userInput for next input
 }
 
+
 const equalsButton = document.querySelector(".equal");
 
 equalsButton.addEventListener("click", () => {
-    handleSecondNumberInput();
-    firstNumber = operate(firstNumber, secondNumber, operator);
-    display.textContent = firstNumber;
+    if (!(operator === "")) {
+        handleSecondNumberInput();
+        firstNumber = operate(firstNumber, secondNumber, operator);
+        operator = ""
+        display.textContent = firstNumber;
+    }
 })
+
 
 const clearButton = document.querySelector(".clear");
 
 clearButton.addEventListener("click", () => {
+    firstNumber = "";
+    userInput = "";
+    secondNumber = "";
+    display.textContent = "0";
+})
 
+const pointButton = document.querySelector(".point")
+
+pointButton.addEventListener("click", () => {
+    if (!userInput.includes(".")) {
+    userInput += ".";
+    display.textContent = userInput;
+    }
+})
+
+const backButton = document.querySelector(".back")
+
+backButton.addEventListener("click", () => {
+    if (userInput) {
+        userInput = userInput.slice(0,-1);
+        if (userInput) {
+            display.textContent = userInput;
+        } else display.textContent = "0";
+    }
 })
